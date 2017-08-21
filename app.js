343d6978fc4792dda6b67402948d6528ab04f499
Sumer Mixon <sumer@ximm.us>
2017-08-21 14:09:39 -0400

    finishing login form validation

--- app.js
+++ app.js
@@ -19,33 +19,67 @@ window.onload = openModal();
  // Login validation //
 //////////////////////
 
-function validateLogin() {
-	// get email, password and submit input
-	var email = document.login.email.value;
-	var password = document.login.password.value;
-	var submit = document.login.submit;
+// Dummy creditials
+var credintials = {
+	email: "sumer@ximm.us",
+	password: "password",
+	remember: false
+};
+
+// Get submit button
+var submit = document.login.submit;
+
+// get email, password and submit input
+var email = document.login.email.value;
+var password = document.login.password.value;
+
+function validateInput() {
+	// update email, password and submit input
+	email = document.login.email.value;
+	password = document.login.password.value;
+	
 
 	// Check if email or password is blank, if not enable submit button 
    	if ((( email != "" ) && ( password != "" ) && ( password.length>=8 ))){
    		submit.disabled = false;
-   	}
+   	} else { submit.disabled = true; }
 }
 
 function validateEmail() {
-	// Get email	
-	var email = document.login.email.value;
+	// Update email and error message	
+	email = document.login.email.value;
+	emailError = document.getElementById('email-error');
 
 	// Validate email 
 	var atpos = email.indexOf("@");
     var dotpos = email.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
-        alert("Not a valid e-mail address");
+        emailError.style.display = "inline";
         return false;
-    }
+    } else emailError.style.display = "none";
 }
 
 // Listen to form input and fire validation
 window.addEventListener('input', function (e) {
  	// console.log("keyup event detected! coming from this element:", e.target);
-	validateLogin();
+	validateInput();
 }, false);
+
+function validateCredintials() {
+	email = document.login.email.value;
+	password = document.login.password.value;
+	remember = document.login.remember.checked; // Checked? true of false
+	
+	var loginError = document.getElementById('login-error');
+	var loginForm = document.getElementById("userLogin");
+
+	if (( email == credintials.email ) && ( password == credintials.password )) {
+		console.log("Success" + remember );
+		closeModal();
+	} else {
+		console.log("Nope");
+		loginError.style.display = "inline";
+		loginForm.reset();
+		submit.disabled = true;
+	}
+}
